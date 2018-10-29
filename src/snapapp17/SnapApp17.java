d99c1534a9e550e1bcd4c62c6b45dd05c9235d53
sendywjynt <sendy.wjynt@gmail.com>
2018-10-29 09:39:31 +0700

    upload pertama

---
+++ src/snapapp17/SnapApp17.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp17;
+
+/**
+ *
+ * @author sendy
+ */
+public class SnapApp17 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+SnapApp17 snap = new SnapApp17();
+snap.setVisible (true);
+    }
+    
+}
